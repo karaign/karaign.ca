de2a0515045887b0e27bb2b9663bd3f704fe79cd
Kara I <kara.ign@icloud.com>
2024-05-05 19:36:12 -0400

    Added SASS support to 11ty config

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,28 @@
+const sass = require("sass");
+const path = require("node:path");
+
 module.exports = function (eleventyConfig) {
 	// Passthrough for static content
 	eleventyConfig.addPassthroughCopy({
 		"./public": "/"
 	})
+	// enable sass
+	eleventyConfig.addTemplateFormats("scss");
+	eleventyConfig.addExtension("scss", {
+		outputFileExtension: "css",
+		compile: async function (inputContent, inputPath) {
+			let parsedPath = path.parse(inputPath);
+
+			let result = sass.compileString(inputContent, {
+				loadPaths: [
+					parsedPath.dir || ".",
+					this.config.dir.includes,
+					"node_modules/@picocss/pico/scss"
+				]
+			});
+			return async (data) => result.css;
+		}
+	})
+
+
 }
